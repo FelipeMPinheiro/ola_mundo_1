1ce1b92a09f5bb9e1137a1b21b997d1778462163
Felipe Moreira Pinheiro <felipe.pinheiro@hotmail.com>
2022-08-07 21:32:21 -0300

    Primeiro comit do codigo

---
+++ src/olamundo/OlaMundo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package olamundo;
+/**
+ * @author Felipe Pinheiro
+ */
+public class OlaMundo {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Ola, Mundo!");
+        //Escreve em tela sem saltar para proxima linha.
+        System.out.println("Ola,Mundo!");
+        //Escreve na tela saltando para proxima linha devido ao final LN
+        
+        //SOUT + tab  ene completa o comando pra vc...
+        //valido para as iniciais das outras linhas.
+    }
+}
